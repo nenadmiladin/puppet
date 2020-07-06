3fb9afcdb7d1b8cdf5f9d1b7305d75f858d916d3
Nenad Miladinovic <nenadmiladin@yahoo.com>
2020-07-06 14:11:55 +0200

    Create local_yum_repo.pp

---
+++ local_yum_repo.pp
@@ -0,0 +1,19 @@
+class local_yum_repo {
+
+  # configure the repo we want to use
+  yumrepo { 'epel_local':
+    name => 'epel_local',
+    descr    => 'Local repo holding company application packages',
+    baseurl  => 'file:///packages/downloaded_rpms',
+    enabled  => 1,
+	  gpgcheck => 0,
+  }
+  package { 'vim-enhanced':
+    ensure => 'installed',
+  }
+
+}
+
+node 'stapp01.stratos.xfusioncorp.com', 'stapp02.stratos.xfusioncorp.com', 'stapp03.stratos.xfusioncorp.com' {
+    include local_yum_repo
+}
