ac19d08ad6b4b1e2623556b6d2f2b504f25aad79
Nenad Miladinovic <nenadmiladin@yahoo.com>
2020-07-12 11:13:04 +0200

    Update passwordless_SSH_KK.pp

--- passwordless_SSH_KK.pp
+++ passwordless_SSH_KK.pp
@@ -8,10 +8,10 @@ class ssh_node1 {
           type   => 'ssh-rsa',
           key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCxNCpKrMkJoNxNID2R8uLOr2sW4AA4GehIL042jmBq5+SaLP9+iM6pJKL/kstJ3a9PezBs5QOclZ5Y9YUUEe2TTYlNPEcmhAhBQrlgfdZYfIUuPgN34QNCPiOXPVX6xZB0faUS/0dWoAU3qZKPmMOnK60/f0f8mvsB4U+X/LTaDARVVCwjiv4AaeL0tOZzU1lix2KCCjJyZlB7cUP7vjrLZo3E5Z+skT5sxHramQ8c3jb6/7RYdGPHxZkacJ28Ov1o8W+qkYzoVOtU5Eh3RqMkKBEpld5Tn5oG7f0wki1Sl0A2pvB0YrzC0sxRhbKiCsZmwo8dESw8PpXW841uX7Ab',
         }
-		    file { '/home/tony/.ssh':
+	file { '/home/tony/.ssh':
           mode => '0700',
         }
-		    file { '/home/tony/.ssh/authorized_keys':
+	file { '/home/tony/.ssh/authorized_keys':
           mode => '0640',
         }
 }
@@ -23,10 +23,10 @@ class ssh_node2 {
           type   => 'ssh-rsa',
           key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCxNCpKrMkJoNxNID2R8uLOr2sW4AA4GehIL042jmBq5+SaLP9+iM6pJKL/kstJ3a9PezBs5QOclZ5Y9YUUEe2TTYlNPEcmhAhBQrlgfdZYfIUuPgN34QNCPiOXPVX6xZB0faUS/0dWoAU3qZKPmMOnK60/f0f8mvsB4U+X/LTaDARVVCwjiv4AaeL0tOZzU1lix2KCCjJyZlB7cUP7vjrLZo3E5Z+skT5sxHramQ8c3jb6/7RYdGPHxZkacJ28Ov1o8W+qkYzoVOtU5Eh3RqMkKBEpld5Tn5oG7f0wki1Sl0A2pvB0YrzC0sxRhbKiCsZmwo8dESw8PpXW841uX7Ab',
         }
-		    file { '/home/steve/.ssh':
+	file { '/home/steve/.ssh':
           mode => '0700',
         }
-		    file { '/home/steve/.ssh/authorized_keys':
+	file { '/home/steve/.ssh/authorized_keys':
           mode => '0640',
         }
     }
@@ -38,10 +38,10 @@ class ssh_node3 {
           type   => 'ssh-rsa',
           key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCxNCpKrMkJoNxNID2R8uLOr2sW4AA4GehIL042jmBq5+SaLP9+iM6pJKL/kstJ3a9PezBs5QOclZ5Y9YUUEe2TTYlNPEcmhAhBQrlgfdZYfIUuPgN34QNCPiOXPVX6xZB0faUS/0dWoAU3qZKPmMOnK60/f0f8mvsB4U+X/LTaDARVVCwjiv4AaeL0tOZzU1lix2KCCjJyZlB7cUP7vjrLZo3E5Z+skT5sxHramQ8c3jb6/7RYdGPHxZkacJ28Ov1o8W+qkYzoVOtU5Eh3RqMkKBEpld5Tn5oG7f0wki1Sl0A2pvB0YrzC0sxRhbKiCsZmwo8dESw8PpXW841uX7Ab',
         }
-		    file { '/home/banner/.ssh':
+	file { '/home/banner/.ssh':
           mode => '0700',
         }
-		    file { '/home/banner/.ssh/authorized_keys':
+	file { '/home/banner/.ssh/authorized_keys':
           mode => '0640',
         }
     }
